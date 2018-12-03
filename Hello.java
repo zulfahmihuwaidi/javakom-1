ee1f4a7f2a428d76f91b0312805d3e376be2d868
huwaaa <muhammad.huwaidi@students.amikom.ac.id>
2018-12-03 16:34:56 +0700

    File Hello.java dan Person.java telah ditambahkan

---
+++ Hello.java
@@ -0,0 +1,18 @@
+public class Hello1 {
+
+	public static void main(String[] args){
+		System.out.println("Hello World!");
+	}
+	
+	private String setMhs(){
+	
+	}
+	
+	public boolean getInfo(int index, String data){
+
+	}
+	
+}
+private class SyncData {}
+
+private class GetMhsActive {}
---
+++ Person.java
@@ -0,0 +1,14 @@
+package bank.account;
+
+public class Person {
+
+	String nama;
+	String noKtp;
+	
+	@Override
+	public String toString() {
+		return "Person [nama=" + nama + ", noKtp=" + noKtp + "]";
+	}
+	
+	
+}
